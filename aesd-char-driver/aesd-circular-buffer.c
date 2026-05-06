6856acd65f821cc38bdc9ee87d3f61b679f08360
Cornelia Schulz <cornelia.schulz@web.de>
2026-05-06 17:52:49 +0200

    switch back to only returning char*

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -66,10 +66,10 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 * Any memory referenced in @param add_entry must be allocated by and/or must have a lifetime managed by the caller.
 */
 // void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
-const struct aesd_buffer_entry* aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
+const char* aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
     // ISO C90 requires all declarations at one place in the beginning
-    const struct aesd_buffer_entry* old_entry = NULL;
+    const char* old_entry = NULL;
 
     // validity of parameter pointers
     if ((buffer == NULL) || (add_entry == NULL))
@@ -77,10 +77,11 @@ const struct aesd_buffer_entry* aesd_circular_buffer_add_entry(struct aesd_circu
 
     // if buffer is already full, remember pointer to the value we are going to overwrite
     if (buffer->full)
-        old_entry = &buffer->entry[buffer->in_offs];
+        old_entry = buffer->entry[buffer->in_offs].buffptr;
 
     // add entry to the buffer and increase in pointer
-    buffer->entry[buffer->in_offs] = *add_entry;
+    buffer->entry[buffer->in_offs].buffptr = add_entry->buffptr;
+    buffer->entry[buffer->in_offs].size = add_entry->size;
     buffer->in_offs = (buffer->in_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
 
     // if buffer was already full, increase out pointer as well
