64acd68c6ef59a304c3405fa9de8f064f1ba7038
Cornelia Schulz <cornelia.schulz@web.de>
2026-05-08 10:43:21 +0200

    uncomment fpos modification in write

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -151,7 +151,7 @@ ssize_t aesd_write(struct file *filp, const char __user *buf, size_t count,
     retval = (count - bytes_not_copied);
 
     // assignment 9) "must set *fpos to *fpos + retcount where retcount is the number of bytes written"
-    *f_pos += retval;
+    // *f_pos += retval;
 
     // if a '\n' is received, insert entry into circular buffer and clear the entry object
     // TODO: can we assume that '\n' occurs only at the end of buf / the entry object?
